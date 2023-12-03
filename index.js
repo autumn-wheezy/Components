35c80b2d7de06f359b83f5dc1a515f0ffbaaa4db
Richard N <152442039+autumn-wheezy@users.noreply.github.com>
2023-12-04 02:54:05 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+// Vue.js with Components
+Vue.component('my-component', {
+    template: '<div>{{ message }}</div>',
+    data: function() {
+        return {
+            message: 'Hello, Vue!'
+        };
+    }
+});
